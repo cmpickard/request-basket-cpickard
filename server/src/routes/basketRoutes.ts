50faf17ecdd18f266d3a1a0373590504eb6cee38
Eric Cho <eric@Erics-MacBook-Air.local>
2026-03-05 16:09:31 -0700

    init commit
    Co-authored-by: Ekerin ekerin.m.a@gmail.com

---
+++ server/src/routes/basketRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as BasketController from '../controllers/basketController.ts';
+
+const router = Router();
+
+router.get('/baskets/:endpoint', (req, res) => {
+
+});
+
+router.post('/:endpoint', (req, res) => {
+
+});
